17d6cef3a083ecb52460b280070d3c2639ab3542
Tobias Schneider <s-tschn3@haw-landshut.com>
2019-12-29 16:52:52 +0100

    Initial commit

---
+++ app/src/main/java/de/hawlandshut/studienprojekt/Kategorieauswahl.java
@@ -0,0 +1,14 @@
+package de.hawlandshut.studienprojekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kategorieauswahl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategorieauswahl);
+    }
+}
